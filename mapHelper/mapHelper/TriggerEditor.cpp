b3060588429c148b82694fc07f017de09071d7e4
月升朝霞 <1134819521@qq.com>
2021-01-07 13:44:28 +0800

    修复创建可破坏物弧度未转化为角度的bug

--- mapHelper/mapHelper/TriggerEditor.cpp
+++ mapHelper/mapHelper/TriggerEditor.cpp
@@ -873,7 +873,7 @@ endfunction
 		if (variableTable.find(id) == variableTable.end()) 
 			continue;
 		
-		sprintf(buffer, "'%.4s',%.1f,%.1f,%.1f,%.1f,%d", unit->name, unit->x, unit->y, unit->angle, unit->scale_x, unit->variation);
+		sprintf(buffer, "'%.4s',%.1f,%.1f,%.1f,%.1f,%d", unit->name, unit->x, unit->y, unit->angle * 180 / 3.14, unit->scale_x, unit->variation);
 		writer.write_string("\tset " + id + " = CreateDestructable(" + std::string(buffer) + ")\n");
 
 		if (unit->doodas_life != 100) {
